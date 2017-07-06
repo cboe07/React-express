0deca09dd6aea4b820d072f816357a7046332732
Chris Boeckel <cboe07@gmail.com>
2017-07-06 10:05:05 -0400

    Add router

--- back-end/app.js
+++ back-end/app.js
@@ -9,6 +9,12 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//Opens the door for people to make AJAX requests to us
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
